d544199ce0dc88865c3ffbb05ece1c4e360077a4
GabrielIGNS <gabrielgranjeia@alunos.utfpr.edu.br>
2023-05-13 05:11:08 -0300

    Requisitos geral pronto e CRUD de jogos

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -57,9 +57,26 @@ export async function getgames(req, res) {
 //     }
 // }
 
-// export async function creategame(req, res) {
-//     res.send("creategame")
-// }
+export async function creategame(req, res) {
+    const {name, image, stockTotal, pricePerDay} = req.body;
+    const validation = await db.query(`
+        SELECT games.name
+            FROM games
+                WHERE games.name = $1;
+        `, [name])
+        const verify = validation.rows[0];
+    if(verify) return res.sendStatus(409);
+    try{
+        const gameId = await db.query(`
+            INSERT INTO games (name, image, "stockTotal", "pricePerDay")
+                VALUES ($1, $2, $3, $4);
+        `, [name, image, stockTotal, pricePerDay])
+        return res.sendStatus(201);
+    }catch(erro) {
+        return res.send(erro.message)
+    }
+    
+}
 
 // export async function deletegame(req, res) {
 //     res.send("deletegame")
--- src/routes/games.routes.js
+++ src/routes/games.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { getgames } from "../controllers/gamesController.js"
+import { getgames, creategame } from "../controllers/gamesController.js"
 // import { creategame, deletegame, editgameById, getgameById, getgames } from "../controllers/gamesController.js"
 import { validateSchema } from "../middlewares/validateSchema.middleware.js"
 import { gamesSchema } from "../schemas/games.schema.js"
@@ -8,7 +8,7 @@ const gamesRouter = Router()
 
 gamesRouter.get("/games", getgames)
 // gamesRouter.get("/games/:id", getgameById)
-// gamesRouter.post("/games", validateSchema(gamesSchema), creategame)
+gamesRouter.post("/games", validateSchema(gamesSchema), creategame)
 // gamesRouter.delete("/games/:id", deletegame)
 // gamesRouter.put("/games/:id", validateSchema(gamesSchema), editgameById)
 
