ff6f4f7ace947cd27cafe3d239e5f2458604740c
GabrielIGNS <gabrielgranjeia@alunos.utfpr.edu.br>
2023-05-13 06:52:56 -0300

    CRUD clientes completo

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -9,28 +9,28 @@ export async function getcustomers(req, res) {
     }
 }
 
-// export async function getgameById(req, res) {
-//     const {id} = req.params;
-//     try{
-//         const gameId = await db.query(`
-//         SELECT *
-// 	    FROM games
-//               WHERE games.id=$1;`, [id]);
+export async function getcustomersById(req, res) {
+    const {id} = req.params;
+    try{
+        const customersId = await db.query(`
+        SELECT *
+	    FROM customers
+              WHERE customers.id=$1;`, [id]);
 
-//         // se precisar que seja apenas 1 registro com algumas infos \/
-//         // const gameIdAll = {
-//         //     ...gameId.rows[0],
-//         //     categorias: gameId.rows.map(gm => gm.categoria)
-//         // }
-//         // delete gameIdAll.categoria;
-//         // res.send(gameIdAll);
-//         // res.send(gameId);
-//         // res.send(gameId.rows);
-//         // res.send(gameId.rows[0]); // esse [0] é para tirar ele de dentro do array, vir só objeto
-//     } catch (erro){
-//         res.send(erro.message)
-//     }
-// }
+        // se precisar que seja apenas 1 registro com algumas infos \/
+        // const gameIdAll = {
+        //     ...gameId.rows[0],
+        //     categorias: gameId.rows.map(gm => gm.categoria)
+        // }
+        // delete gameIdAll.categoria;
+        // res.send(gameIdAll);
+        // res.send(gameId);
+        // res.send(gameId.rows);
+        return res.send(customersId.rows[0]); // esse [0] é para tirar ele de dentro do array, vir só objeto
+    } catch (erro){
+       return res.send(erro.message)
+    }
+}
 
 // export async function getgameById(req, res) {
 //     const {id} = req.params;
@@ -83,6 +83,32 @@ export async function createcustomers(req, res) {
 //     res.send("deletegame")
 // }
 
-// export async function editgameById(req, res) {
-//     res.send("editgameById")
-// }
+export async function editcustomersById(req, res) {
+    const {id} = req.params;
+    const {name, phone, cpf, birthday} = req.body;
+    try{
+        const validation = await db.query(`
+        SELECT customers.cpf, customers.id
+            FROM customers
+                WHERE customers.cpf = $1 AND NOT customers.id = $2;
+        `, [cpf, id])
+        const verify = validation.rows[0];
+        if(verify) return res.sendStatus(409);
+        const customersId = await db.query(`
+        UPDATE customers
+            SET "name" = $1, phone = $2, cpf = $3, birthday = $4
+                WHERE id=$5;`, [name, phone, cpf, birthday ,id]);
+        // se precisar que seja apenas 1 registro com algumas infos \/
+        // const gameIdAll = {
+        //     ...gameId.rows[0],
+        //     categorias: gameId.rows.map(gm => gm.categoria)
+        // }
+        // delete gameIdAll.categoria;
+        // res.send(gameIdAll);
+        // res.send(gameId);
+        // res.send(gameId.rows);
+        return res.sendStatus(200); // esse [0] é para tirar ele de dentro do array, vir só objeto
+    } catch (erro){
+       return res.send(erro.message)
+    }
+}
--- src/routes/customers.routes.js
+++ src/routes/customers.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { getcustomers, createcustomers } from "../controllers/customersController.js"
+import { getcustomers, createcustomers, getcustomersById, editcustomersById } from "../controllers/customersController.js"
 // import { creategame, deletegame, editgameById, getgameById, getgames } from "../controllers/gamesController.js"
 import { validateSchema } from "../middlewares/validateSchema.middleware.js"
 import { customersSchema } from "../schemas/customers.schema.js"
@@ -7,9 +7,9 @@ import { customersSchema } from "../schemas/customers.schema.js"
 const customersRouter = Router()
 
 customersRouter.get("/customers", getcustomers)
-// gamesRouter.get("/games/:id", getgameById)
+customersRouter.get("/customers/:id", getcustomersById)
 customersRouter.post("/customers", validateSchema(customersSchema), createcustomers)
 // gamesRouter.delete("/games/:id", deletegame)
-// gamesRouter.put("/games/:id", validateSchema(gamesSchema), editgameById)
+customersRouter.put("/customers/:id", validateSchema(customersSchema), editcustomersById)
 
 export default customersRouter;
