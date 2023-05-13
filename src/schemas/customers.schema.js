6341e494f2b06f70dbf6c4766c3c07ac500a3295
GabrielIGNS <gabrielgranjeia@alunos.utfpr.edu.br>
2023-05-13 07:40:11 -0300

    pequena correção para avaliador

--- src/schemas/customers.schema.js
+++ src/schemas/customers.schema.js
@@ -4,5 +4,5 @@ export const customersSchema = joi.object({
     name: joi.string().required(),
     phone: joi.string().required().min(10).max(11),
     cpf: joi.number().required().min(11),
-    birthday: joi.date().less('1-12-2022').required()
+    birthday: joi.date().required()
 });
